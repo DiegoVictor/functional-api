1bc141e481eae72c2531fe74c014e4e3b1681fd3
Diego Victor <diegovictorgonzaga@gmail.com>
2022-03-27 01:23:44 -0300

    Create endpoint to retrieve one feature flag

--- src/index.ts
+++ src/index.ts
@@ -9,4 +9,17 @@ app.get('/featureFlags', async (_, response) => {
   response.json(flags);
 });
 
+app.get('/featureFlags/:key', async (request, response) => {
+  const { key } = request.params;
+  const flag = await featureFlagController.getOneByKey(key);
+
+  if (flag) {
+    return response.json(flag);
+  }
+
+  return response.status(404).json({
+    message: 'Feature flag not found',
+  });
+});
+
 app.listen(process.env.PORT);
