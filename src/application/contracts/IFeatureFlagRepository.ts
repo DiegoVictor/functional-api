b98bc54341d614f6ef9a24f0a79af0d605b17b28
Diego Victor <diegovictorgonzaga@gmail.com>
2022-03-27 01:21:11 -0300

    Create repository method to get one by key

--- src/application/contracts/IFeatureFlagRepository.ts
+++ src/application/contracts/IFeatureFlagRepository.ts
@@ -2,4 +2,5 @@ import { IFeatureFlag } from './IFeatureFlag';
 
 export interface IFeatureFlagRepository {
   getAll(): Promise<IFeatureFlag[]>;
+  getOneByKey(key: string): Promise<IFeatureFlag | undefined>;
 }
--- src/infra/repositories/featureFlag.ts
+++ src/infra/repositories/featureFlag.ts
@@ -5,3 +5,9 @@ const flags: IFeatureFlag[] = [];
 export const getAll = async (): Promise<IFeatureFlag[]> => {
   return flags;
 };
+
+export const getOneByKey = async (
+  key: string
+): Promise<IFeatureFlag | undefined> => {
+  return flags.find((flag) => flag.key === key);
+};
