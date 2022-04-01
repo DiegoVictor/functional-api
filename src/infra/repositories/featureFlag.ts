2b83a8987eb463955affd4e0b70ef908d699675e
Diego Victor <diegovictorgonzaga@gmail.com>
2022-04-01 17:06:58 -0300

    Mock one feature flag

--- src/infra/repositories/featureFlag.ts
+++ src/infra/repositories/featureFlag.ts
@@ -1,6 +1,13 @@
+import crypto from 'crypto';
 import { IFeatureFlag } from '../../application/contracts/IFeatureFlag';
 
-const flags: IFeatureFlag[] = [];
+const flags: IFeatureFlag[] = [
+  {
+    id: crypto.randomUUID(),
+    active: true,
+    key: 'encode_output',
+  },
+];
 
 export const getAll = async (): Promise<IFeatureFlag[]> => {
   return flags;
