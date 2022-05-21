5442d652c8a0eb18cb52f2c45810b49b9fdb6b83
Diego Victor <diegovictorgonzaga@gmail.com>
2022-05-21 10:47:17 -0300

    Resolve dependency

---
+++ src/application/services/resolve.ts
@@ -0,0 +1,16 @@
+import { factories } from '../../infra/factories';
+
+const registry = new Map();
+
+export const resolve = <T>(name: string): T => {
+  if (registry.has(name)) {
+    return registry.get(name);
+  }
+
+  if (!factories[name] || typeof factories[name] === 'function') {
+    throw new Error(`No factory found for ${name}`);
+  }
+  registry.set(name, factories[name]());
+
+  return registry.get(name);
+};
