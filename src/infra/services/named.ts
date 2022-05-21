c1828466ac57f017aab9b25cea1abb351c49c90b
Diego Victor <diegovictorgonzaga@gmail.com>
2022-05-21 10:47:01 -0300

    Type service methods

--- src/infra/services/named.ts
+++ src/infra/services/named.ts
@@ -1,10 +1,11 @@
 import axios from 'axios';
+import { INameService } from '../../application/contracts/INameService';
 
 const api = axios.create({
   baseURL: process.env.NAMED_BASE_URL,
 });
 
-export const getNames = async (): Promise<string[]> =>
+export const getNames: INameService['getNames'] = async (): Promise<string[]> =>
   api
     .get('/names', {
       params: {
