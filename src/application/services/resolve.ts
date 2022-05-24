17ced034874633d7c866d9126b21008ad7cec44c
Diego Victor <diegovictorgonzaga@gmail.com>
2022-05-24 18:35:55 -0300

    Update resolver

--- src/application/services/resolve.ts
+++ src/application/services/resolve.ts
@@ -2,12 +2,14 @@ import { factories } from '../../infra/factories';
 
 const registry = new Map();
 
+export const clear = () => registry.clear();
+
 export const resolve = <T>(name: string): T => {
   if (registry.has(name)) {
     return registry.get(name);
   }
 
-  if (!factories[name] || typeof factories[name] === 'function') {
+  if (!factories[name] || typeof factories[name] !== 'function') {
     throw new Error(`No factory found for ${name}`);
   }
   registry.set(name, factories[name]());
