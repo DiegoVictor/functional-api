d1123c79ceecf6ce23efdf6920b3d1293907a57d
Diego Victor <diegovictorgonzaga@gmail.com>
2022-06-05 00:05:38 -0300

    Fix imports

--- src/config/dependencies.ts
+++ src/config/dependencies.ts
@@ -1,7 +1,7 @@
 import { INameService } from '@application/contracts/INameService';
 import { IFeatureFlagRepository } from '@application/contracts/IFeatureFlagRepository';
-import { featureFlatRepository } from 'src/frameworks/repositories';
-import * as namedService from 'src/frameworks/services/named';
+import { featureFlatRepository } from '@frameworks/repositories';
+import * as namedService from '@frameworks/services/named';
 
 export const dependencies: Record<string, () => any> = {
   FeatureFlagRepository: (): IFeatureFlagRepository => featureFlatRepository,
