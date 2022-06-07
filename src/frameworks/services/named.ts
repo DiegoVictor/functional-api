bfcd8fd8f56900c12787ea7648c54b81c2c3cc85
Diego Victor <diegovictorgonzaga@gmail.com>
2022-06-07 19:51:53 -0300

    Use hardcoded values

--- src/frameworks/services/named.ts
+++ src/frameworks/services/named.ts
@@ -1,18 +1,16 @@
 import axios from 'axios';
-import * as functions from 'firebase-functions';
 
 import { INameService } from '@application/contracts/INameService';
 
-const { named_base_url, named_dataset_id } = functions.config();
 const api = axios.create({
-  baseURL: named_base_url,
+  baseURL: 'https://named-api.herokuapp.com',
 });
 
 export const getNames: INameService['getNames'] = async (): Promise<string[]> =>
   api
     .get('/names', {
       params: {
-        dataset: named_dataset_id,
+        dataset: '1642602568240',
       },
     })
     .then(({ data }) => data);
