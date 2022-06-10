4f002932da205e5c7c359eb216b0eb217334ea37
Diego Victor <diegovictorgonzaga@gmail.com>
2022-06-10 00:52:36 -0300

    Update repository path

--- src/config/dependencies.ts
+++ src/config/dependencies.ts
@@ -1,6 +1,6 @@
 import { INameService } from '@application/contracts/INameService';
 import { IFeatureFlagRepository } from '@application/contracts/IFeatureFlagRepository';
-import { featureFlatRepository } from '@frameworks/repositories';
+import { featureFlatRepository } from '@frameworks/firebase/repositories';
 import * as namedService from '@frameworks/services/named';
 
 export const dependencies: Record<string, () => any> = {
